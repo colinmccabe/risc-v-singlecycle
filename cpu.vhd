76abca2ff912582c15fff2072fb2b7953ad5c08f
colinmccabe <colin@localhost>
2014-12-06 12:16:48 -0500

    kill pc+8 instr when jumping

--- cpu.vhd
+++ cpu.vhd
@@ -10,11 +10,14 @@ end cpu;
 architecture Synthesizable of cpu is
 
    constant NOP : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
+   constant INSTR_ADDR_WIDTH : integer := 14;
+   constant DATA_ADDR_WIDTH : integer := 13;
 
    signal inst1, prog_mem_out : STD_LOGIC_VECTOR(31 downto 0);
    signal inst2 : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
-   signal pc : STD_LOGIC_VECTOR(14 downto 0) := (others => '0');
-   signal jmp_or_branch_addr, data_addr : STD_LOGIC_VECTOR(14 downto 0);
+   signal pc : STD_LOGIC_VECTOR(INSTR_ADDR_WIDTH-1 downto 0) := (others => '0');
+   signal jmp_or_branch_addr : STD_LOGIC_VECTOR(INSTR_ADDR_WIDTH-1 downto 0);
+   signal data_addr : STD_LOGIC_VECTOR(DATA_ADDR_WIDTH-1 downto 0);
    
    signal rf_out_x, rf_out_y, rf_data_in : STD_LOGIC_VECTOR(31 downto 0);
    signal x, y : STD_LOGIC_VECTOR(31 downto 0) := (others => '0');
@@ -43,7 +46,7 @@ architecture Synthesizable of cpu is
    signal I_type_signed, S_type : STD_LOGIC;
    signal do_jump : STD_LOGIC;
    
-   signal stall_2nd_cycle : STD_LOGIC := '0';
+   signal stall_2nd_cycle, just_jumped : STD_LOGIC := '0';
    
    signal I_imm, S_imm : STD_LOGIC_VECTOR(11 downto 0);
    signal SB_imm : STD_LOGIC_VECTOR(12 downto 0);
@@ -56,7 +59,7 @@ architecture Synthesizable of cpu is
       PORT (
          clka : IN STD_LOGIC;
          ena : IN STD_LOGIC;
-         addra : IN STD_LOGIC_VECTOR(12 DOWNTO 0);
+         addra : IN STD_LOGIC_VECTOR(INSTR_ADDR_WIDTH-3 DOWNTO 0);
          douta : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
       );
    END COMPONENT;
@@ -65,7 +68,7 @@ architecture Synthesizable of cpu is
       PORT (
          clka : IN STD_LOGIC;
          wea : IN STD_LOGIC_VECTOR(0 DOWNTO 0);
-         addra : IN STD_LOGIC_VECTOR(12 DOWNTO 0);
+         addra : IN STD_LOGIC_VECTOR(DATA_ADDR_WIDTH-3 DOWNTO 0);
          dina : IN STD_LOGIC_VECTOR(31 DOWNTO 0);
          douta : OUT STD_LOGIC_VECTOR(31 DOWNTO 0)
       );
@@ -109,7 +112,7 @@ architecture Synthesizable of cpu is
 begin
    -- Control signals
    
-   inst1 <= NOP when do_jump = '1' else
+   inst1 <= NOP when do_jump = '1' or just_jumped = '1' else
             prog_mem_out;
    
    opcode <= inst2(6 downto 0);
@@ -202,7 +205,7 @@ begin
                            when branch = '1' else
                          STD_LOGIC_VECTOR(SIGNED(pc) + resize(SIGNED(UJ_imm), pc'length) - 8)
                            when jal = '1' else
-                         alu_out(14 downto 0); -- jalr
+                         alu_out(INSTR_ADDR_WIDTH-1 downto 0); -- jalr
 
    -- Memory stall
    stall_1st_cycle <= (load or (stor and (not storing_word))) and (not stall_2nd_cycle);
@@ -210,7 +213,7 @@ begin
    -- Memory signals
    data_mem_we(0) <= (stor and stall_2nd_cycle) or storing_word;
    prog_mem_en <= not stall_1st_cycle; -- Don't fetch inst on 1st cycle of stall
-   data_addr <= alu_out(14 downto 0);
+   data_addr <= alu_out(DATA_ADDR_WIDTH-1 downto 0);
    byte_to_stor <= stor_data_reg(7 downto 0);
    hw_to_stor <= stor_data_reg(15 downto 0);
 
@@ -253,14 +256,14 @@ begin
    Inst_prog_mem : prog_mem PORT MAP (
       clka => clk,
       ena => prog_mem_en,
-      addra => pc(14 downto 2),
+      addra => pc(INSTR_ADDR_WIDTH-1 downto 2),
       douta => prog_mem_out
    );
   
    Inst_data_mem : data_mem PORT MAP (
       clka => clk,
       wea => data_mem_we,
-      addra => data_addr(14 downto 2),
+      addra => data_addr(DATA_ADDR_WIDTH-1 downto 2),
       dina => stor_data,
       douta => load_data_word
    );
@@ -293,8 +296,10 @@ begin
             if do_jump = '1' then
                inst2 <= inst1;
                pc <= jmp_or_branch_addr;
+               just_jumped <= '1';
             elsif stall_1st_cycle = '1' then
                stall_2nd_cycle <= '1';
+               just_jumped <= '0';
             else
                inst2 <= inst1;
                stor_data_reg <= rf_out_y;
@@ -304,6 +309,7 @@ begin
                y <= y_next;
 
                stall_2nd_cycle <= '0';
+               just_jumped <= '0';
             end if;
          end if;
       end process;
